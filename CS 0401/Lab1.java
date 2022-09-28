f2ec653180787950750dbc75f52ae6eb3dda692f
EthanD57 <114536214+EthanD57@users.noreply.github.com>
2022-09-27 20:10:20 -0400

    Add files via upload

---
+++ CS 0401/Lab1.java
@@ -0,0 +1,57 @@
+import java.io.*; // I/O
+import java.util.*; // Scanner class
+ 
+public class Lab1
+{
+    public static void main( String args[] ) 
+    {
+        final double MILES_PER_MARATHON = 26.21875; // i.e 26 miles 285 yards
+ 
+        Scanner input = new Scanner (System.in);
+ 
+        // THE FOLLOWING THREE VARIABLES ARE PRINTED OUT AT THE END OF THE PROGRAM
+        double aveMPH=0, aveMinsPerMile=0, aveSecsPerMile=0;
+ 
+        // YOUR JOB IS TO DO WHAT YOU HAVE TO TO TO PUT THE CORRECT VALUES INTO THEM
+        // BEFORE THEY GET PRINTED OUT AT THE BOTTOM
+ 
+        System.out.print("Enter marathon time in hrs minutes seconds: "); // i.e. 3 49 37
+        // DO NOT WRITE OR MODIFY ANYTHING ABOVE THIS LINE
+ 
+         
+        //  - - - - - - - - - - - A L L   Y O U R   C O D E   H E R E - - - - - - - - - - - - - - - 
+        int h = input.nextInt();
+        int m = input.nextInt();
+        int s = input.nextInt();
+ 
+        double total_seconds = 0.0;
+         
+        total_seconds += ((double)h*3600) + ((double)m*60) + ((double)s);
+ 
+        double secondsPerMiles = total_seconds / MILES_PER_MARATHON;
+ 
+        aveSecsPerMile = secondsPerMiles % 60;
+ 
+        /*
+         
+        aveMinsPerMile = (secondsPerMiles / 60);
+         
+        This was the code causing the issue
+        */
+ 
+        aveMinsPerMile = (int)(secondsPerMiles / 60);
+ 
+ 
+        total_seconds /= 3600;
+ 
+        aveMPH = MILES_PER_MARATHON / total_seconds;
+ 
+ 
+        // DO NOT WRITE OR MODIFY ANYTHING BELOW THIS LINE. JUST LET MY CODE PRINT THE VALUES YOU PUT INTO THE 3 VARS
+        System.out.println();
+        System.out.format("Average MPH was: %.2f mph\n", aveMPH  );
+        System.out.format("Average mile split was %.0f mins %.1f seconds per mile", aveMinsPerMile, aveSecsPerMile );
+        System.out.println();
+ 
+    } // END MAIN METHOD
+} // EOF
---
+++ CS 0401/Lab2.java
@@ -0,0 +1,79 @@
+
+
+import java.io.*; // BufferedReader
+import java.util.*; // Scanner
+
+public class Lab2
+{
+	public static void main (String args[]) throws Exception // i.e. the input file you put on cmd line is not in directory
+	{
+		// ALWAYS TEST FIRST TO VERIFY USER PUT REQUIRED INPUT FILE NAME ON THE COMMAND LINE
+		if (args.length < 1 )
+		{
+			System.out.println("\nusage: C:\\> java Lab2 <input filename>\n\n"); // i.e. C:\> java Lab2 L2input.txt
+			System.exit(0);
+		}
+		BufferedReader infile = new BufferedReader (new FileReader( args[0] )); // we read our text file line by line
+		int lineNum=0;
+		while( infile.ready() ) // ifile.ready() means "while there are any lines left in file"
+		{
+			String line = toAlphaLowerCase(infile.readLine()); 
+			if ( isPalindrome( line ) )
+				System.out.format("<%s> IS palindrome.\n",line);
+			else
+				System.out.format("<%s> NOT palindrome.\n",line);
+		}
+	} // END MAIN
+	
+	// ******* MODIFY NOTHING ABOVE  THIS   LINE YOU FILL IN THE METHODS BELOW *******	
+	// RETURNS A STRING WITH ALL NON ALPHABETIC CHARS REMOVED. ALL REMAINING ARE ALPHAS CONVERTED TO LOWER CASE
+	// "Madam I'm Adam" returns "madamimadam" which is now ready for a simple palindromic test
+	// To test whether a char is alpha i.e. letter of the alphabet 
+	//
+	// READ THIS BEFORE WRITING THIS METHOD!  ==> https://docs.oracle.com/javase/tutorial/i18n/text/charintro.html
+	
+	static String toAlphaLowerCase( String s )
+	{
+		String str = "";
+		for (int i=0; i < s.length(); i++)
+		{
+			char x = s.charAt(i);
+			if (Character.isLetter(x))
+			{
+				str += x;
+			}
+		}
+		str = str.toLowerCase();
+		return str; 
+	}
+	// RETURNs true if and only if the string passed in is a palindrome
+	static boolean isPalindrome( String s )
+	{
+		boolean value = true;
+		for (int i=0; i < s.length(); i++)
+		{
+			if (s.charAt(i)==s.charAt(s.length()-1-i))
+			{
+				continue;
+			}
+			else
+			{
+				value = false;
+				break;
+			}
+		}
+		return value;
+		
+	}
+} // END LAB2 CLASS
+
+
+
+
+
+
+
+
+
+
+
