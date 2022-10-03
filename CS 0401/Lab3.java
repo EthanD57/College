ec9aebdf630980ccf237dba80f3cfab2dff928f3
EthanD57 <ethandefilippi@gmail.com>
2022-10-03 17:57:11 -0400

    Lab3

---
+++ CS 0401/Lab3.java
@@ -0,0 +1,54 @@
+// STARTER FILE LAB #3 
+
+import java.util.*;
+import java.io.*;
+
+public class Lab3
+{
+    static final int MAX_CAPACITY = 30;  // HARDOCED PLENTY BIG. WE'LL DO TRIM AFTER
+    
+    public static void main( String args[] ) throws Exception
+    {
+        if (args.length < 1 )
+        {
+            System.out.println("usage: $ java Lab3 <inupt file of random numbers>\n");
+            System.exit(0);
+        }
+        
+        int[] arr = new int[ MAX_CAPACITY ];
+        int count=0;
+        
+        Scanner infile = new Scanner( new File( args[0] ) );
+        while ( infile.hasNextInt() )
+        {
+            insertInOrder( arr, count, infile.nextInt() );
+            ++count;
+        }
+        
+        arr = trimArray( arr, count );
+        printArray( arr ); // NOTE:  NO COUNT PASSED IN
+        
+    }// END MAIN
+    
+    // ############################################################################################################
+    
+    // MUST USE ENHANCED FOR LOOP IN THIS METHOD
+    // (YOUR TRIM BETTER HAVE BEEN WRITEN CORRECTLY)
+    static void printArray( int[] array )
+    {
+        // PRINT EACH NUMBER WITH A SPACE AFTER IT
+        System.out.println(); // LEAVE THIS HERE
+    }
+    
+    static int[] trimArray( int[] array, int count  )
+    {
+        return array; // THIS IS NOT CORRECT. YOU MUST REPLACE IT!!!!
+    }
+    
+   
+    // THE CODE IN HERE NOW JUST APPENDS. THIS IS NOT CORRECT
+    static void insertInOrder( int[] arr, int count, int newVal   )
+    {
+        arr[count] = newVal; // THIS IS NOT CORRECT. YOU MUST REPLACE IT!!
+    }
+}// END LAB #3
