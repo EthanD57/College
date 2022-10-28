ebe3fca837acdb49e35054dad2e2e76a2d1bfeb4
EthanD57 <ethandefilippi@gmail.com>
2022-10-28 16:24:31 -0400

    Final version of project 4

--- CS 0401/Project4.java
+++ CS 0401/Project4.java
@@ -2,32 +2,30 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Project4 {
     public static void main(String[] args) throws Exception
     {
         if (args.length < 2 )
 		{
-			System.out.println("\nusage: C:\\> java Lab2 <input filename> <input dictionary>\n\n");
+			System.out.println("\nusage: C:\\> java Lab2 <input Dictionary> <input Jumbles>\n\n");
 			System.exit(0);
 		}
         ArrayList<String>Jumbles = new ArrayList<String>();
         ArrayList<String>Dictionary = new ArrayList<String>();
         ArrayList<String>CanonDictionary = new ArrayList<String>();
-        ArrayList<String>CanonJumbles = new ArrayList<String>();
 
 //This is the buffered readers for the dictionary and jumbles files -------------------------
 
-        try (BufferedReader input = new BufferedReader (new FileReader( args[0] )))
+        try (BufferedReader input = new BufferedReader (new FileReader( args[1] )))
         {
             while (input.ready())
             {
-                String curJumble = input.readLine();
-                Jumbles.add(curJumble);
-                CanonJumbles.add(canon(curJumble));
+                Jumbles.add(input.readLine());
             }
         }
-        try (BufferedReader input2 = new BufferedReader (new FileReader( args[1] )))
+        try (BufferedReader input2 = new BufferedReader (new FileReader( args[0] )))
         {
             while (input2.ready())
             {
@@ -37,10 +35,11 @@ public class Project4 {
             }
         }
 //---------------------------------------------------------------------------------------------
+       Collections.sort(Jumbles);
        
         for (int i=0; i<Jumbles.size(); i++)
         {
-            System.out.println(Jumbles.get(i) + " " + findMatches(CanonJumbles.get(i), CanonDictionary, Dictionary));
+            System.out.println(Jumbles.get(i) + " " + findMatches(canon(Jumbles.get(i)), CanonDictionary, Dictionary));
         }
 
     }
@@ -60,16 +59,23 @@ public class Project4 {
 //This method finds the matches for the jumbles and returns them as a string ----------------------------
     public static String findMatches (String word, ArrayList<String>Canondict,ArrayList<String>Dictionary)
     {
-        String matches = "";
+        ArrayList<String>Matches = new ArrayList<String>();
         for (int i = 0; i < Canondict.size(); i++)
             {
                 if (word.equals(Canondict.get(i)))
                 {
-                    matches += Dictionary.get(i) + " ";
+                    Matches.add(Dictionary.get(i));
                 }
             }
 
-        return matches;
-    }
+        Collections.sort(Matches);
 
+        String matchString = "";
+        for (String s: Matches)
+        {
+            matchString += s + " ";
+        }
+        return matchString;
+    
+    }
 }
