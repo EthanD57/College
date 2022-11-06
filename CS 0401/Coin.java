777d50a449eb69b8dccbf31114584785976fd524
EthanD57 <ethandefilippi@gmail.com>
2022-11-06 18:20:49 -0500

    Finished Lab6 AKA Coin.java

---
+++ CS 0401/Coin.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class Coin
+{
+    private int NumHeads;
+    private int NumTails;
+    private Random Random;
+
+    public Coin (int seed) 
+    {
+        NumHeads = 0;
+        NumTails = 0;
+        this.Random = new Random(seed);
+    }
+    
+    public String flip()
+    {
+        if (this.Random.nextInt(2) == 1)
+        {
+            NumHeads++;
+            return "H";
+        }
+        else
+        {
+            NumTails++;
+            return "T";
+        }
+    }
+    
+    public int getNumHeads() 
+    {
+        return this.NumHeads;
+    }
+    
+    public int getNumTails()
+    {
+        return this.NumTails;
+    }
+
+    public void reset()
+    {
+        this.NumHeads = 0;
+        this.NumTails = 0;
+    }
+}
