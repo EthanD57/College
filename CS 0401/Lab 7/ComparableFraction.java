cd9959996c14c786855028d675e423add6bb4a68
EthanD57 <ethandefilippi@gmail.com>
2022-11-14 20:26:00 -0500

    Finished Lab 7 for CS 0401 and added folders to keep track of things.

---
+++ CS 0401/Lab 7/ComparableFraction.java
@@ -0,0 +1,66 @@
+public class ComparableFraction implements Comparable<ComparableFraction>
+{
+	private int numer;
+	private int denom;
+
+	// ACCESSORS (GETTERS)
+	public int getNumer()
+	{	return numer;
+	}
+	public int getDenom()
+	{	return denom;
+	}
+	// MUTATORS (SETTERS)
+	public void setNumer( int n )
+	{	numer = n;
+	}
+	public void setDenom( int d )
+	{
+		if (d==0) { System.out.println("Can't have 0 in denom"); System.exit(0); }
+		else denom=d;
+	}
+	// FULL CONSTRUCTOR - an arg for each class data member
+	public ComparableFraction ( int n, int d )
+	{	int gcd = gcd( n, d );
+		setNumer(n/gcd);
+		setDenom(d/gcd);
+	}
+	private int gcd( int n, int d )
+	{	int gcd = n<d ? n : d; // same as::  if (n<d) gcd=n; else gcd=d;
+		while( gcd > 0 ) // NOT EFFICIENT AS EUCLID BUT SIMPLE
+			if (n%gcd==0 && d%gcd==0) 
+				return gcd; 
+			else --gcd;
+		return 1; // they were co-prime no GCD except 1 :(
+	}
+	// COPY CONSTRUCTOR - takes ref to some already initialized Comparable<ComparableFraction> object
+	public ComparableFraction ( ComparableFraction other )
+	{
+		this( other.getNumer(), other.getDenom() ); // call my full C'Tor with other Fraction's data
+	}
+	// REQUIRED BY THE COMPARABLE INTERFACE 
+	// if this == other return 0; if this>other return 1; else return -1
+	public int compareTo( ComparableFraction other )
+	{
+		ComparableFraction diff = this.subtract( other );
+		if (diff.getNumer() > 0 && diff.getDenom() > 0) return 1;
+		else if (diff.getNumer() < 0 || diff.getDenom() <0) return -1;
+		return 0;
+		// you are only allowed to define one variable in this method
+		// that variable should be a Fraction whose value is this - other  
+		// HINT: copy in your subtract() method from project 7 and RE-USE it not rewrite it 
+		// now you can just examin the numer and denom of your diff fraction
+		// to determine that fraction is postive negative or 0
+		// return 0 -1 or 1 accordingly
+		// NO OTHER VARIABLES Of ANY KIND
+		// NO DOUBLES, NO CASTING
+	}
+	public ComparableFraction subtract(ComparableFraction that) 
+	{
+		return new ComparableFraction((this.getNumer() * that.getDenom() - this.getDenom() * that.getNumer()),(this.getDenom() * that.getDenom()));
+	}
+	public String toString()
+	{
+		return getNumer() +  "/" + getDenom() + "\t=" +  + ((double)getNumer()/(double)getDenom()); 
+	}
+}// EOF
