0dfc182c6eac6bef6993c324dcfa155724479c5f
EthanD57 <114536214+EthanD57@users.noreply.github.com>
2023-03-27 20:16:42 -0400

    Lab8 for CS 0445

---
+++ CS 0445/Lab8/TowerTester.java
@@ -0,0 +1,22 @@
+package Lab8;
+import java.io.*;
+import java.util.*;
+
+public class TowerTester
+{
+	public static void main( String args[] ) throws Exception
+	{
+		Tower<Integer> myTower = new Tower<Integer>(); 
+		Scanner infile = new Scanner( new FileReader( args[0] ) ); // CONTAINS 1 LINE SEQUENCE LIKE 10 9 8 7 6 5 4 3 2 1 
+		while ( infile.hasNextInt() )
+			myTower.push( infile.nextInt() );
+		infile.close();
+		
+		System.out.format( "myTower loaded from %s: %s\n", args[0], myTower );
+		
+		while ( ! myTower.empty() )
+		{	myTower.pop();
+			System.out.format( "myTower after pop: %s\n", myTower );
+		}
+	} // END MAIN
+} // END OF TOWERTESTER CLASS
