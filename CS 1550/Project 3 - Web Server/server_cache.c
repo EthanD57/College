0fd6cf317764d577c7354af9819b79ee24680e4b
Ethan Defilippi <ethandefilippi@gmail.com>
2024-11-19 23:50:09 -0500

    Final commits for project 3 and 4 for cs1550. Project 3 is a web server that uses a cache to store web pages. Project 4 is a do-it-yourself semaphore implementation in the linux kernel.

--- CS 1550/Project 3 - Web Server/server_cache.c
+++ CS 1550/Project 3 - Web Server/server_cache.c
@@ -16,21 +16,51 @@
 
 #define CACHE_LIMIT 5
 
+#define THREAD_POOL_SIZE 100
+#define MAX_QUEUE_SIZE 1000
+
+// Cache structures (unchanged)
 typedef struct {
     char filename[1024];
     char* content;
     size_t content_length;
 } CachedFile;
 
+// Thread pool structures
+typedef struct {
+    int connfd;
+    int is_valid;
+} Task;
+
+typedef struct {
+    Task* tasks;
+    int front;
+    int rear;
+    int count;
+    int size;
+    pthread_mutex_t mutex;
+    pthread_cond_t not_empty;
+    pthread_cond_t not_full;
+} TaskQueue;
+
+typedef struct {
+    pthread_t* threads;
+    TaskQueue* queue;
+    int thread_count;
+    int is_running;
+} ThreadPool;
+
+// Global variables
 CachedFile cache[CACHE_LIMIT];
 int cache_start = 0;
 int cache_count = 0;
 pthread_mutex_t cache_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-FILE *stats_file; 
+FILE *stats_file;
 int cache_index = 0;
+ThreadPool* thread_pool;
 
-CachedFile* amendCache(char *filename){
+CachedFile* ammendCache(char *filename){
 
     FILE *recentFile = fopen(filename, "rb");
     if (recentFile == NULL){
@@ -69,6 +99,50 @@ CachedFile* fetchCached(const char *filename) {
     return NULL;  // File not found in cache
 }
 
+TaskQueue* createTaskQueue(int size) {
+    TaskQueue* queue = (TaskQueue*)malloc(sizeof(TaskQueue));
+    queue->tasks = (Task*)malloc(sizeof(Task) * size);
+    queue->size = size;
+    queue->front = 0;
+    queue->rear = -1;
+    queue->count = 0;
+    pthread_mutex_init(&queue->mutex, NULL);
+    pthread_cond_init(&queue->not_empty, NULL);
+    pthread_cond_init(&queue->not_full, NULL);
+    return queue;
+}
+
+void enqueueTask(TaskQueue* queue, Task task) {
+    pthread_mutex_lock(&queue->mutex);
+    
+    while (queue->count >= queue->size) {
+        pthread_cond_wait(&queue->not_full, &queue->mutex);
+    }
+    
+    queue->rear = (queue->rear + 1) % queue->size;
+    queue->tasks[queue->rear] = task;
+    queue->count++;
+    
+    pthread_cond_signal(&queue->not_empty);
+    pthread_mutex_unlock(&queue->mutex);
+}
+
+Task dequeueTask(TaskQueue* queue) {
+    pthread_mutex_lock(&queue->mutex);
+    
+    while (queue->count == 0) {
+        pthread_cond_wait(&queue->not_empty, &queue->mutex);
+    }
+    
+    Task task = queue->tasks[queue->front];
+    queue->front = (queue->front + 1) % queue->size;
+    queue->count--;
+    
+    pthread_cond_signal(&queue->not_full);
+    pthread_mutex_unlock(&queue->mutex);
+    
+    return task;
+}
 
 void response(int connfd)
 {
@@ -126,7 +200,7 @@ void response(int connfd)
         }
     }
     if (cached == NULL){ // File not found in cache
-        cached = amendCache(filename); // Add file to cache
+        cached = ammendCache(filename); // Add file to cache
         if (cached != NULL){ 
             localCopy = malloc(sizeof(CachedFile));
             *localCopy = *cached;
@@ -218,67 +292,104 @@ void response(int connfd)
     pthread_exit(NULL);
 }
 
-int main()
-{
+void* worker_thread(void* arg) {
+    ThreadPool* pool = (ThreadPool*)arg;
+    
+    while (pool->is_running) {
+        Task task = dequeueTask(pool->queue);
+        if (task.is_valid) {
+            response(task.connfd);
+        }
+    }
+    
+    return NULL;
+}
+
+ThreadPool* createThreadPool(int thread_count) {
+    ThreadPool* pool = (ThreadPool*)malloc(sizeof(ThreadPool));
+    pool->thread_count = thread_count;
+    pool->is_running = 1;
+    pool->queue = createTaskQueue(MAX_QUEUE_SIZE);
+    pool->threads = (pthread_t*)malloc(sizeof(pthread_t) * thread_count);
+    
+    for (int i = 0; i < thread_count; i++) {
+        pthread_create(&pool->threads[i], NULL, worker_thread, pool);
+    }
+    
+    return pool;
+}
+
+void destroyThreadPool(ThreadPool* pool) {
+    pool->is_running = 0;
+    
+    // Signal all threads to wake up
+    for (int i = 0; i < pool->thread_count; i++) {
+        Task dummy_task = {-1, 0};
+        enqueueTask(pool->queue, dummy_task);
+    }
+    
+    // Wait for all threads to finish
+    for (int i = 0; i < pool->thread_count; i++) {
+        pthread_join(pool->threads[i], NULL);
+    }
+    
+    // Cleanup
+    pthread_mutex_destroy(&pool->queue->mutex);
+    pthread_cond_destroy(&pool->queue->not_empty);
+    pthread_cond_destroy(&pool->queue->not_full);
+    free(pool->queue->tasks);
+    free(pool->queue);
+    free(pool->threads);
+    free(pool);
+}
+
+int main() {
     stats_file = fopen("stats_cache.txt", "w");
-    // Sockets represent potential connections
-    // We make an internet socket
+    
     int sfd = socket(PF_INET, SOCK_STREAM, 0);
-    if (-1 == sfd)
-    {
+    if (-1 == sfd) {
         perror("Cannot create socket\n");
         exit(EXIT_FAILURE);
     }
 
-    // We will configure it to use this machine's IP, or
-    // for us, localhost (127.0.0.1)
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
-
     addr.sin_family = AF_INET;
-    // Web servers always listen on port 80
     addr.sin_port = htons(80);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    // So we bind our socket to port 80
-    if (-1 == bind(sfd, (struct sockaddr *)&addr, sizeof(addr)))
-    {
+    if (-1 == bind(sfd, (struct sockaddr *)&addr, sizeof(addr))) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // And set it up as a listening socket with a backlog of 10 pending connections
-    if (-1 == listen(sfd, 10))
-    {
+    if (-1 == listen(sfd, 10)) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    // A server's gotta serve...
-    for (;;)
-    {
-        // accept() blocks until a client connects. When it returns,
-        // we have a client that we can do client stuff with.
+    // Create thread pool
+    thread_pool = createThreadPool(THREAD_POOL_SIZE);
+
+    // Main server loop
+    while (1) {
         int connfd = accept(sfd, NULL, NULL);
-        if (connfd < 0)
-        {
+        if (connfd < 0) {
             perror("Accept failed");
-            exit(EXIT_FAILURE);
+            continue;
         }
 
-        // At this point a client has connected. The remainder of the
-        // loop is handling the client's GET request and producing
-        // our response.
-
-        // THREAD TIME
-
-        pthread_t thread;
-        pthread_create(&thread, NULL, response, (void *) connfd);
-        pthread_detach(thread);
+        // Create task and add to queue
+        Task task = {connfd, 1};
+        enqueueTask(thread_pool->queue, task);
     }
+
+    // Cleanup (this part won't be reached in the current implementation)
+    destroyThreadPool(thread_pool);
     fclose(stats_file);
     close(sfd);
     pthread_mutex_destroy(&lock);
     pthread_mutex_destroy(&cache_lock);
+    
     return 0;
 }
--- CS 1550/Project 3 - Web Server/tester.py
+++ CS 1550/Project 3 - Web Server/tester.py
@@ -5,6 +5,8 @@ url2 = "http://127.0.0.1/test2.txt"
 url3 = "http://127.0.0.1/test3.txt"
 url4 = "http://127.0.0.1/test4.txt"
 url5 = "http://127.0.0.1/test5.txt"
+url6 = "http://127.0.0.1/test6.txt"
+
 
 for i in range(100):
     response = requests.get(url)
@@ -12,5 +14,8 @@ for i in range(100):
     response = requests.get(url3)
     response = requests.get(url4)
     response = requests.get(url5)
+    response = requests.get(url6)
+
+
 
 
